9bc09acaeab6e8bcb2ffb93d7d9837c6ed7d375d
Jan Hoepfner <jan-laucha@gmx.de>
2020-11-10 16:39:26 +0100

    Erweiterung dfs um eine Suche bis zu gegeben Endknoten.

--- include/analytics/dfs.hpp
+++ include/analytics/dfs.hpp
@@ -17,6 +17,13 @@ namespace analytics{
     std::vector<relationship*> dfs(graph_db_ptr& graph, node::id_t start);
 
     /*
+        Bekommt eine Referenz auf einen Graphen, die ID des Startknotens sowie ein Array mit ID's für Endknoten Übergeben.
+        Führt eine Tiefensuche durch, die alle vom Startknoten aus erreichbaren Endknoten findet.
+        Rückgabe ist hierbei ein Vector der alle durchlaufenen Kanten enthält, oder wenn es keinen Weg gibt ein leerer Vector.
+    */
+    std::vector<relationship*> dfs(graph_db_ptr& graph, node::id_t start, std::vector<node::id_t>& end);
+
+    /*
         Diese Funktion wird von dfs verwendet. Sollte man also eine normale Tiefensuche durchführen wollen, ist es ratsam dfs zu verwenden.
 
         Diese Funktion bekommt eine Referenz auf einen Graphen, die ID des Startknotens, ein FoundNodes Objekt sowie einen Vector vom Typ relationship* übergeben.
@@ -25,6 +32,16 @@ namespace analytics{
         Rückgabe ist hierbei ein Vector der alle durchlaufenen Kanten enthält, welche sich hierbei im partial_vec befinden.
     */
     void dfs_recursive (graph_db_ptr& graph, node::id_t start, utils::FoundNodes& found, std::vector<relationship*>& partial_vec);
+
+    /*
+        Diese Funktion wird von dfs verwendet. Sollte man also eine normale Tiefensuche durchführen wollen, ist es ratsam dfs zu verwenden.
+
+        Diese Funktion bekommt eine Referenz auf einen Graphen, die ID des Startknotens, ein FoundNodes Objekt, einen Vector vom Typ relationship*, einen Vektor mit ID's von Endknoten sowie die Referenz auf eine Variable die angibt, ob ein Endknoten gefunden wurde übergeben.
+        Hierbei sind Standardmäßig alle Werte im Vector des FoundNodes-Objektes auf false gesetzt und der Vector vom Typ relationship* ist leer.
+        Führt eine Tiefensuche durch, die alle vom Startknoten aus erreichbaren Endknoten findet.
+        Rückgabe ist hierbei ein Vector der alle durchlaufenen Kanten enthält, welche sich hierbei im partial_vec befinden.
+    */
+    void dfs_recursive (graph_db_ptr& graph, node::id_t start, utils::FoundNodes& found, std::vector<relationship*>& partial_vec,std::vector<node::id_t>& end, bool& found_endnode);
 
 }
 
--- src/analytics/dfs.cpp
+++ src/analytics/dfs.cpp
@@ -14,13 +14,33 @@ std::vector<relationship*> analytics::dfs(graph_db_ptr& graph, node::id_t start)
 } 
 
 /*
-        Diese Funktion wird von dfs verwendet. Sollte man also eine normale Tiefensuche durchführen wollen, ist es ratsam dfs zu verwenden.
+  Bekommt eine Referenz auf einen Graphen, die ID des Startknotens sowie ein Array mit ID's für Endknoten Übergeben.
+  Führt eine Tiefensuche durch, die alle vom Startknoten aus erreichbaren Endknoten findet.
+  Rückgabe ist hierbei ein Vector der alle durchlaufenen Kanten enthält, oder wenn es keinen Weg gibt ein leerer Vector.
+*/
+std::vector<relationship*> analytics::dfs(graph_db_ptr& graph, node::id_t start, std::vector<node::id_t>& end){
+  bool found_endnode = false;
+  utils::FoundNodes found = utils::FoundNodes(graph->get_nodes()->as_vec().capacity());
+  std::vector<relationship*> partial_vec = {};
+  if(std::find(end.begin(), end.end(), start) == end.end()){
+    found.found_nodes[start] = true;
+    dfs_recursive(graph, start, found, partial_vec, end, found_endnode);
+  }
+  if(found_endnode){
+    return partial_vec;
+  } else{
+    return {};
+  }
+} 
 
-        Diese Funktion bekommt eine Referenz auf einen Graphen, die ID des Startknotens, ein FoundNodes Objekt sowie einen Vector vom Typ relationship* übergeben.
-        Hierbei sind Standardmäßig alle Werte im Vector des FoundNodes-Objektes auf false gesetzt und der Vector vom Typ relationship* ist leer.
-        Führt eine Tiefensuche durch, die alle vom Startknoten aus erreichbaren Knoten findet.
-        Rückgabe ist hierbei ein Vector der alle durchlaufenen Kanten enthält, welche sich hierbei im partial_vec befinden.
-    */
+/*
+  Diese Funktion wird von dfs verwendet. Sollte man also eine normale Tiefensuche durchführen wollen, ist es ratsam dfs zu verwenden.
+
+  Diese Funktion bekommt eine Referenz auf einen Graphen, die ID des Startknotens, ein FoundNodes Objekt sowie einen Vector vom Typ relationship* übergeben.
+  Hierbei sind Standardmäßig alle Werte im Vector des FoundNodes-Objektes auf false gesetzt und der Vector vom Typ relationship* ist leer.
+  Führt eine Tiefensuche durch, die alle vom Startknoten aus erreichbaren Knoten findet.
+  Rückgabe ist hierbei ein Vector der alle durchlaufenen Kanten enthält, welche sich hierbei im partial_vec befinden.
+*/
 void analytics::dfs_recursive (graph_db_ptr& graph, node::id_t start, utils::FoundNodes& found, std::vector<relationship*>& partial_vec){
   graph->foreach_from_relationship_of_node(graph->node_by_id(start),[&] (relationship& r) {
         node::id_t to_node = r.to_node_id();
@@ -30,4 +50,31 @@ void analytics::dfs_recursive (graph_db_ptr& graph, node::id_t start, utils::Fou
             dfs_recursive(graph, to_node, found, partial_vec);
         }
     });
+}
+
+/*
+  Diese Funktion wird von dfs verwendet. Sollte man also eine normale Tiefensuche durchführen wollen, ist es ratsam dfs zu verwenden.
+
+  Diese Funktion bekommt eine Referenz auf einen Graphen, die ID des Startknotens, ein FoundNodes Objekt, einen Vector vom Typ relationship*, einen Vektor mit ID's von Endknoten sowie die Referenz auf eine Variable die angibt, ob ein Endknoten gefunden wurde übergeben.
+  Hierbei sind Standardmäßig alle Werte im Vector des FoundNodes-Objektes auf false gesetzt und der Vector vom Typ relationship* ist leer.
+  Führt eine Tiefensuche durch, die alle vom Startknoten aus erreichbaren Endknoten findet.
+  Rückgabe ist hierbei ein Vector der alle durchlaufenen Kanten enthält, welche sich hierbei im partial_vec befinden.
+*/
+void analytics::dfs_recursive (graph_db_ptr& graph, node::id_t start, utils::FoundNodes& found, std::vector<relationship*>& partial_vec,std::vector<node::id_t>& end, bool& found_endnode){
+  if(found_endnode){
+    return;
+  } else{
+    graph->foreach_from_relationship_of_node(graph->node_by_id(start),[&] (relationship& r) {
+      node::id_t to_node = r.to_node_id();
+       if((!found.found_nodes[to_node]) && (!found_endnode)){
+        found.found_nodes[to_node] = true;
+        partial_vec.push_back(&r);
+        if(std::find(end.begin(), end.end(), to_node) != end.end()){
+          found_endnode = true;
+        }else{
+          dfs_recursive(graph, to_node, found, partial_vec, end, found_endnode);
+        }
+      }
+    });
+  }
 }
