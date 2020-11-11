69e468a79b0b7e1159148de8648358c925229119
Jan Hoepfner <jan-laucha@gmx.de>
2020-11-11 16:56:42 +0100

    Umbenennung found_nodes zu PropertyTracker.

---
+++ include/utils/property_tracker.hpp
@@ -0,0 +1,36 @@
+#ifndef PROPERTY_TRACKER_HPP
+#define PROPERTY_TRACKER_HPP
+
+#include <vector>
+
+namespace utils{
+
+    /*
+        Dient zur Verwaltung von Eigenschaften auf den Knoten.
+    */
+   template<typename T>
+    struct PropertyTracker{
+
+        /*
+            ist der Vector vom Typ T. Hier werden die Eigenschaften gespeichert,
+            und es kann mittels der node_id auf die gespeicherten Eigenschaften zugegriffen werden.
+        */
+        std::vector<T> propertys;
+
+        /*
+            initialisiert den Vector mit der gegebenen Länge. Alle Einträge sind zu Begin der übergebene default_value.
+        */
+        PropertyTracker(int number_of_nodes, T default_value);
+    };
+
+
+    /*
+    initialisiert den Vector mit der gegebenen Länge. Alle Einträge sind zu Begin der übergebene default_value.
+    */
+    template<typename T>
+    PropertyTracker<T>::PropertyTracker(int number_of_nodes, T default_value){
+        propertys = std::vector<T>(number_of_nodes, default_value);
+    } 
+}
+
+#endif //PROPERTY_TRACKER_HPP
--- src/analytics/bfs_.cpp
+++ src/analytics/bfs_.cpp
@@ -6,20 +6,20 @@
   Rückgabe ist hierbei ein Vector der alle durchlaufenen Kanten enthält. 
 */
 std::vector<relationship*> analytics::bfs(graph_db_ptr& graph, node::id_t start){
-    utils::FoundNodes<bool> found = utils::FoundNodes<bool>(graph->get_nodes()->as_vec().capacity(), false);
+    utils::PropertyTracker<bool> found = utils::PropertyTracker<bool>(graph->get_nodes()->as_vec().capacity(), false);
     std::queue<node::id_t> active_nodes;
     std::vector<relationship*> result;
 
     active_nodes.push(start);
-    found.found_nodes[start] = true;
+    found.propertys[start] = true;
 
     while((!active_nodes.empty())){
     node::id_t active_n = active_nodes.front();
     active_nodes.pop();
     graph->foreach_from_relationship_of_node(graph->node_by_id(active_n),[&] (relationship& r) {
         node::id_t to_node = r.to_node_id();
-        if((!found.found_nodes[to_node])){
-            found.found_nodes[to_node] = true;
+        if((!found.propertys[to_node])){
+            found.propertys[to_node] = true;
             result.push_back(&r);
             active_nodes.push(to_node);
         }
@@ -35,12 +35,12 @@ std::vector<relationship*> analytics::bfs(graph_db_ptr& graph, node::id_t start)
 */
 std::vector<relationship*> analytics::bfs(graph_db_ptr& graph, node::id_t start, std::vector<node::id_t>& end){
     bool found_end_node = false;
-    utils::FoundNodes<bool> found = utils::FoundNodes<bool>(graph->get_nodes()->as_vec().capacity(), false);
+    utils::PropertyTracker<bool> found = utils::PropertyTracker<bool>(graph->get_nodes()->as_vec().capacity(), false);
     std::queue<node::id_t> active_nodes;
     std::vector<relationship*> result;
 
     active_nodes.push(start);
-    found.found_nodes[start] = true;
+    found.propertys[start] = true;
 
     if(std::find(end.begin(), end.end(), start) == end.end()){
         while((!active_nodes.empty()) && (!found_end_node)){
@@ -51,8 +51,8 @@ std::vector<relationship*> analytics::bfs(graph_db_ptr& graph, node::id_t start,
             if(std::find(end.begin(), end.end(), to_node) != end.end()){
                 result.push_back(&r);
                 found_end_node = true;
-            } else if((!found.found_nodes[to_node]) && (!found_end_node)){
-                found.found_nodes[to_node] = true;
+            } else if((!found.propertys[to_node]) && (!found_end_node)){
+                found.propertys[to_node] = true;
                 result.push_back(&r);
                 active_nodes.push(to_node);
             }
